75a87ea451b1a5a23e43b1f504d25d2c3dc6f392
helena <helena.gilja@gmail.com>
2023-04-02 14:14:27 +0100

    env, tailwind.js, vite.js for config added

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from '../src/redux/store.js';
-import App from './App';
 
 import './index.css';
+import App from './App';
+import { store } from './redux/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <Router>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </Router>
+  </Provider>
+</React.StrictMode>,
 );
 
