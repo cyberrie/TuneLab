a3aa3070c51e8764dda2ff2147b9d2ae3a9dc4d2
helena <helena.gilja@gmail.com>
2023-04-02 17:55:17 +0100

    vite config updated so i can use env variables, shazamCore also updated

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,14 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-require("dotenv").config();
 
-const X_RapidAPI_Key = process.env.X_RapidAPI_Key;
+const X_RapidAPI_Key = import.meta.env.VITE_X_RAPIDAPI_KEY;
 
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://shazam-core.p.rapidapi.com/v1",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", X_RapidAPI_Key);
+      if (X_RapidAPI_Key) {
+        headers.set("X-RapidAPI-Key", X_RapidAPI_Key);
+      }
       return headers;
     },
   }),
